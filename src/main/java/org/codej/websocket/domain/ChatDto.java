3c6e98e4b5dd53c5106c1883a5d0969c7c9e585f
younjaeyeal <jyyoun1021@gmail.com>
2022-09-17 19:03:14 +0900

    채팅 DTO,채팅방 DTO 수정

--- src/main/java/org/codej/websocket/domain/ChatDto.java
+++ src/main/java/org/codej/websocket/domain/ChatDto.java
@@ -8,9 +8,12 @@ import lombok.Data;
 @Builder
 public class ChatDto {
 
-    //메시지 타입 :  입장 채팅
+    // 메시지 타입 :  입장 채팅
+    // 메시지 타입에 따라서 동작하는 구조가 달라진다.
+    // 입장과 퇴장 ENTER 과 LEAVE 의 경우 입장/퇴장 이벤트 처리가 실행되고,
+    // TALK 는 말 그대로 해당 채팅방을 sub 하고 있는 모든 클라이언트에게 전달됩니다.
     public enum MessageType{
-        ENTER, TALK
+        ENTER, TALK,LEAVE
     }
     private MessageType type; //메시지 타입
     private String roomId;// 방 번호
