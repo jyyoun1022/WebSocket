8f878fc313fdf3d61931a67949ee48ca949cd207
younjaeyeal <jyyoun1021@gmail.com>
2022-09-17 13:24:02 +0900

    ChatController

---
+++ src/main/java/org/codej/websocket/controller/ChatController.java
@@ -0,0 +1,18 @@
+package org.codej.websocket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ChatController {
+
+    @GetMapping("/chat")
+    public String chatGet(){
+
+        log.info("@ChatController, chat GET()");
+
+        return "chat";
+    }
+}
